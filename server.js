47f6abdca49ddab665ef2e5f4a70b1698befc92e
Sebastian Wegner <sebastian.wegner@ardentcode.com>
2022-01-10 15:54:54 +0100

    files moved to folder public

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
   let extname = path.extname(filePath);
   let contentType = 'text/html';
 
@@ -28,10 +32,13 @@ const server = http.createServer((req, res) => {
   fs.readFile(filePath, (err, content) => {
     if (err) {
       if (err.code === 'ENOENT') {
-        fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(content, 'utf8');
-        });
+        fs.readFile(
+          path.join(__dirname, 'public', '404.html'),
+          (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf8');
+          }
+        );
       } else {
         res.writeHead(500);
         res.end(`Server Error: ${err.code}`);
