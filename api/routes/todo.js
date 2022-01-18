f936684827893633d1dd2dd53f081073cb7b8273
Sebastian Wegner <sebastian.wegner@ardentcode.com>
2022-01-18 09:46:39 +0100

    adding and deleting tasks in list arrays working without 500 error

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -14,7 +14,27 @@ router.patch('/', async (req, res) => {
         },
       }
     );
-    await listModel.save();
+    res.status(200).send('Successfully added');
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    await listModel.findOneAndUpdate(
+      {
+        label: 'To do',
+      },
+      {
+        $pull: {
+          items: {
+            id: parseInt(req.params.id),
+          },
+        },
+      }
+    );
+    res.status(200).send('Successfully deleted');
   } catch (err) {
     res.status(500).send(err);
   }
